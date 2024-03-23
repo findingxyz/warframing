5d6d21fe781685c21a096cd23ea50afb166175ff
findingxyz <21078718+findingxyz@users.noreply.github.com>
2024-03-24 03:09:22 +1030

    unique page for unique mod

---
+++ website/src/routes/mod/[...rest]/+page.server.js
@@ -0,0 +1,5 @@
+export function load({ params }) {
+	const post = params.rest;
+
+	return { post };
+}
---
+++ website/src/routes/mod/[...rest]/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { onMount } from 'svelte';
+
+    import RelativeModShow from '../../../lib/RelativeModShow.svelte'
+
+	export let data;
+
+	let mod;
+   
+	const query = '/' + data.post;
+
+	onMount(async () => {
+        const res = await fetch('/api/mod?q=' + encodeURIComponent(query));
+        mod = await res.json();
+    });
+</script>
+
+{#if mod}
+	<RelativeModShow showFusing=true modData={mod} />
+{/if}
